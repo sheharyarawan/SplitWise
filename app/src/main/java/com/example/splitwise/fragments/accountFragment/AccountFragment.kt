3246d9558b23b8c7a56ded6960e5eb589e76a8eb
sheharyar <sheharyarawan1153@gmail.com>
2026-09-10 23:25:14 +0500

    fragments for bottom nav added and theri layout inflated  with fragment

---
+++ app/src/main/java/com/example/splitwise/fragments/accountFragment/AccountFragment.kt
@@ -0,0 +1,13 @@
+package com.example.splitwise.fragments.accountFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.splitwise.R
+
+
+class AccountFragment : Fragment(R.layout.fragment_account) {
+
+}
---
+++ app/src/main/java/com/example/splitwise/fragments/friendFragment/FriendsFragment.kt
@@ -0,0 +1,13 @@
+package com.example.splitwise.fragments.friendFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.splitwise.R
+
+
+class FriendsFragment : Fragment(R.layout.fragment_friends) {
+
+}
