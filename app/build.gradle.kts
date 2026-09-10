2392c20d7c8ee441e903e3153274779ce0ad7d82
sheharyar <sheharyarawan1153@gmail.com>
2026-09-10 22:36:19 +0500

    Project Initialized (libraries and plugins imported

---
+++ app/build.gradle.kts
@@ -0,0 +1,119 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.hilt)
+    id("androidx.navigation.safeargs.kotlin")
+}
+android {
+    namespace = "com.example.splitwise"
+    compileSdk {
+        version = release(37)
+    }
+
+    defaultConfig {
+        applicationId = "com.example.splitwise"
+        minSdk = 24
+        targetSdk = 37
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding= true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.constraintlayout)
+
+    // Material
+    implementation(libs.material)
+
+
+    // =========================
+    // Lifecycle / ViewModel
+    // =========================
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+
+    // =========================
+    // Coroutines + Flow
+    // =========================
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+
+    // =========================
+    // Navigation
+    // =========================
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+
+    // =========================
+    // Room
+    // =========================
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+
+    ksp(libs.androidx.room.compiler)
+
+
+    // =========================
+    // Retrofit
+    // =========================
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+
+    // OkHttp logging
+    implementation(libs.okhttp.logging.interceptor)
+
+
+    // =========================
+    // Hilt
+    // =========================
+
+    implementation(libs.hilt.android)
+
+    ksp(libs.hilt.compiler)
+
+
+    // =========================
+    // Tests
+    // =========================
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.kotlin.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.3" apply false
+}
