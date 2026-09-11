afa54cf40114b5212c39c67805b2efb666113c9d
sheharyar <sheharyarawan1153@gmail.com>
2026-09-11 20:40:00 +0500

    xml created for group fragment

--- app/src/main/java/com/example/splitwise/fragments/groupFragment/GroupsFragment.kt
+++ app/src/main/java/com/example/splitwise/fragments/groupFragment/GroupsFragment.kt
@@ -6,7 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.splitwise.R
+import com.example.splitwise.databinding.ActivityMainBinding
 
 class GroupsFragment : Fragment(R.layout.fragment_groups) {
+    lateinit var binding: ActivityMainBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
 
 }
