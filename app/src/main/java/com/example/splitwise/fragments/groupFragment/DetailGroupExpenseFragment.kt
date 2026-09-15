fc95110cbfc465168674bd356d251c033b15e5ef
sheharyar <sheharyarawan1153@gmail.com>
2026-09-15 19:37:49 +0500

    expense detail distribution xml made

---
+++ app/src/main/java/com/example/splitwise/fragments/groupFragment/DetailGroupExpenseFragment.kt
@@ -0,0 +1,11 @@
+package com.example.splitwise.fragments.groupFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.splitwise.R
+
+class DetailGroupExpenseFragment : Fragment(R.layout.fragment_detail_group_expense) {
+}
