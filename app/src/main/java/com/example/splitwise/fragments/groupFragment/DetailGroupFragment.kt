36b9bfdfe08975460e64584dc8c4db1c74b4fb05
sheharyar <sheharyarawan1153@gmail.com>
2026-09-16 18:01:41 +0500

    group setting xml made

--- app/src/main/java/com/example/splitwise/fragments/groupFragment/DetailGroupFragment.kt
+++ app/src/main/java/com/example/splitwise/fragments/groupFragment/DetailGroupFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.splitwise.R
+import com.example.splitwise.databinding.FragmentDetailGroupBinding
 
 
 class DetailGroupFragment : Fragment(R.layout.fragment_detail_group) {
+
+    lateinit var binding: FragmentDetailGroupBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding= FragmentDetailGroupBinding.bind(view)
+        binding.detailGroupSettings.setOnClickListener {
+            findNavController().navigate(R.id.action_detailGroupFragment_to_groupSettingsFragment)
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/splitwise/fragments/groupFragment/GroupSettingsFragment.kt
@@ -0,0 +1,13 @@
+package com.example.splitwise.fragments.groupFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.splitwise.R
+
+
+class GroupSettingsFragment : Fragment(R.layout.fragment_group_settings) {
+
+}
