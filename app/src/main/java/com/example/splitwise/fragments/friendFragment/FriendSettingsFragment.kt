c843122a92062fac7bc56577e82eb1572cd301c9
sheharyar <sheharyarawan1153@gmail.com>
2026-09-19 04:40:58 +0500

    used app tint

--- app/src/main/java/com/example/splitwise/fragments/friendFragment/FriendSettingsFragment.kt
+++ app/src/main/java/com/example/splitwise/fragments/friendFragment/FriendSettingsFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.splitwise.R
+import com.example.splitwise.databinding.FragmentFriendSettingsBinding
 
 
 class FriendSettingsFragment : Fragment(R.layout.fragment_friend_settings) {
 
-
+    lateinit var binding: FragmentFriendSettingsBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        binding= FragmentFriendSettingsBinding.bind(view)
+        binding.friendSettingToolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
