0ecb070f853e90d414fa6458e3050ec24692edb0
sheharyar <sheharyarawan1153@gmail.com>
2026-09-19 01:44:31 +0500

    fragment friend setting xml added

--- app/src/main/java/com/example/splitwise/fragments/friendFragment/FriendDetailFragment.kt
+++ app/src/main/java/com/example/splitwise/fragments/friendFragment/FriendDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.splitwise.R
 import com.example.splitwise.databinding.FragmentFriendDetailBinding
 
@@ -15,5 +16,16 @@ class FriendDetailFragment : Fragment(R.layout.fragment_friend_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding= FragmentFriendDetailBinding.bind(view)
+        setUpToolbarClicks()
+
+    }
+
+    fun setUpToolbarClicks(){
+        binding.friendDetailSettingsIcon.setOnClickListener {
+            findNavController().navigate(R.id.action_friendDetailFragment_to_friendSettingsFragment)
+        }
+        binding.friendDetailBackIcon.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
---
+++ app/src/main/java/com/example/splitwise/fragments/friendFragment/FriendSettingsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.splitwise.fragments.friendFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.splitwise.R
+
+
+class FriendSettingsFragment : Fragment(R.layout.fragment_friend_settings) {
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
