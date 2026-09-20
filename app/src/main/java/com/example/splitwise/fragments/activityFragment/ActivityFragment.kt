3bdd814cc8cecbc84a8d11b5c79b8652c8835b29
sheharyar <sheharyarawan1153@gmail.com>
2026-09-21 02:08:57 +0500

    activity xml made

--- app/src/main/java/com/example/splitwise/fragments/activityFragment/ActivityFragment.kt
+++ app/src/main/java/com/example/splitwise/fragments/activityFragment/ActivityFragment.kt
@@ -5,9 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.splitwise.MainActivity
 import com.example.splitwise.R
-
+import com.example.splitwise.databinding.FragmentActivityBinding
 
 class ActivityFragment : Fragment(R.layout.fragment_activity) {
 
+    lateinit var binding: FragmentActivityBinding
+
+    override fun onResume() {
+        super.onResume()
+        (activity as MainActivity).showAddExpenseButton()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding= FragmentActivityBinding.bind(view)
+    }
 }
