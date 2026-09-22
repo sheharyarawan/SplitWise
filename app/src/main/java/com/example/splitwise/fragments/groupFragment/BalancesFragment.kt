089dbfd69d64997d64effe0ae3c9abd341ed8190
sheharyar <sheharyarawan1153@gmail.com>
2026-09-22 23:36:59 +0500

    group balances xml

---
+++ app/src/main/java/com/example/splitwise/fragments/groupFragment/BalancesFragment.kt
@@ -0,0 +1,12 @@
+package com.example.splitwise.fragments.groupFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.splitwise.R
+
+class BalancesFragment : Fragment() {
+
+}
